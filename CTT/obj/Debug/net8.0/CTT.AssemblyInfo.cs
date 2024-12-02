af6827cf44dbddbf5dd2be1aba02b8cc19e53d1f
Ksenia <nikkseniyu@gmail.com>
2024-12-02 22:48:40 +0700

    02/12/24

--- CTT/obj/Debug/net8.0/CTT.AssemblyInfo.cs
+++ CTT/obj/Debug/net8.0/CTT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CTT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0efd825fab0e5b02ae471e15d748a345b12cde97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a332425db8099ee7e3b6c99ab7552f096e4dc7a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CTT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CTT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
